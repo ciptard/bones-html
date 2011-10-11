181e25017b92fa3f94235aebc504e7f620d15d57
eddiemachado <machado08@gmail.com>
2011-10-11 16:23:46 -0500

    first commit
    Hooray!

---
+++ library/js/scripts.js
@@ -0,0 +1,31 @@
+/*
+Bones Scripts File
+Author: Eddie Machado
+*/
+
+// Modernizr.load loading the right scripts only if you need them
+Modernizr.load([
+	{
+    // Let's see if we need to load selectivizr
+    test : Modernizr.borderradius,
+    // Modernizr.load loads selectivizr for IE6-8
+    nope : ['library/js/libs/selectivizr-min.js']
+	}
+]);
+
+/* imgsizer (flexible images for fluid sites) */
+var imgSizer={Config:{imgCache:[],spacer:"/path/to/your/spacer.gif"},collate:function(aScope){var isOldIE=(document.all&&!window.opera&&!window.XDomainRequest)?1:0;if(isOldIE&&document.getElementsByTagName){var c=imgSizer;var imgCache=c.Config.imgCache;var images=(aScope&&aScope.length)?aScope:document.getElementsByTagName("img");for(var i=0;i<images.length;i++){images[i].origWidth=images[i].offsetWidth;images[i].origHeight=images[i].offsetHeight;imgCache.push(images[i]);c.ieAlpha(images[i]);images[i].style.width="100%";}
+if(imgCache.length){c.resize(function(){for(var i=0;i<imgCache.length;i++){var ratio=(imgCache[i].offsetWidth/imgCache[i].origWidth);imgCache[i].style.height=(imgCache[i].origHeight*ratio)+"px";}});}}},ieAlpha:function(img){var c=imgSizer;if(img.oldSrc){img.src=img.oldSrc;}
+var src=img.src;img.style.width=img.offsetWidth+"px";img.style.height=img.offsetHeight+"px";img.style.filter="progid:DXImageTransform.Microsoft.AlphaImageLoader(src='"+src+"', sizingMethod='scale')"
+img.oldSrc=src;img.src=c.Config.spacer;},resize:function(func){var oldonresize=window.onresize;if(typeof window.onresize!='function'){window.onresize=func;}else{window.onresize=function(){if(oldonresize){oldonresize();}
+func();}}}}
+
+// as the page loads, cal these scripts
+$(document).ready(function() {
+
+	
+	
+	// add all your scripts here
+
+ 
+}); /* end of as page load scripts */
